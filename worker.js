58a829da51f9e8873f3be0354c92a1098964be91
Keilyx-12 <thefakeemail976@gmail.com>
2026-09-14 00:43:21 +0800

    Simplify fetch method in worker.js

--- worker.js
+++ worker.js
@@ -1,12 +1,5 @@
 export default {
   async fetch(request, env, ctx) {
-    try {
-      if (env.ASSETS) {
-        return await env.ASSETS.fetch(request);
-      }
-      return new Response("Assets binding not configured.", { status: 500 });
-    } catch (err) {
-      return new Response(`Worker Error: ${err.message}`, { status: 500 });
-    }
+    return env.ASSETS.fetch(request);
   }
 };
